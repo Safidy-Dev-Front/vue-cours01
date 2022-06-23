a0b839246f29692875ab55f4498c94b15e6cbe6d
Safidi <safidiniainaromino13@gmail.com>
2022-06-23 11:31:45 +0300

    feat: template syntax

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,26 +1,48 @@
 <template>
     <div>
-       <h1>{{ message }}</h1> 
-       <p>{{ value }}</p>
-       <button @click="toIncrement">incremennt {{count}}</button>
+        <h1>{{ message }}</h1>
+        <p>{{ value }}</p>
+        <button @click="toIncrement" :disabled="isButtonDisabled">incremennt {{ countId }}</button>
     </div>
+    <ul>
+        <UserTodo userItem=1 />
+        <UserTodo userItem=2 />
+        <UserTodo userItem=3 />
+    </ul>
+    <!-- v-html -->
+    <div v-html="vHtml"></div>
+    <!-- Attribute Bindings -->
+    <div :id="countId">ID is {{countId}}</div>
 </template>
 
 
 <script>
- export default{
+import UserTodo from '@/components/todoStatic.vue';
+export default {
     name: 'FirstComponent',
+    components:{
+        UserTodo
+    },
     props: ["value"],
-    data(){
+    data() {
         return {
             message: "Salut les gens",
-            count: 0
+            countId: 0,
+           vHtml: `<h1>Test Vhtmpl</h1>`
         };
     },
     methods: {
         toIncrement() {
-            this.count++;
+            this.countId++;
+        },
+    },
+    computed: {
+        isButtonDisabled(){
+            if(this.countId >= 20){
+                return true
+            }
+            return false
         }
     }
- }
+}
 </script>
---
+++ src/components/todoStatic.vue
@@ -0,0 +1,12 @@
+ <template>
+    <li>User {{userItem }}</li>
+ </template>
+ 
+ <script>
+ export default{
+     name: 'UserTodo',
+    props: {
+        userItem:Number
+    }
+ }
+ </script>
